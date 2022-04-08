ff81e2258d4ab4c600d0a53fa9e6a3b027f9a04a
Jacopo Angelini <jacopo.angelini@live.it>
2022-04-08 19:02:23 +0200

    rifiniture

--- js/script.js
+++ js/script.js
@@ -27,6 +27,8 @@ function randomizza() {
   }
 }
 
+
+
 $(() => {
   randomizza();
   $('.images').hide();
@@ -39,20 +41,23 @@ $(() => {
 
 
   $('.cards').click(function() {
+    console.log('aaa');
     opacity = $(this).css('opacity');
     $(this).children().show();
 
     if (passaggio == 0 && opacity != 0.5) {
       card1 = $(this).children().prop('src');
       posCard1 = $(this).prop('id');
-      card1 = card1.substr(26);
+      console.log(posCard1);
+      card1 = card1.substr(27);
       clicks++;
       passaggio++;
     } else if (passaggio == 1 && opacity != 0.5) {
       posCard2 = $(this).prop('id');
+      console.log(posCard2);
       if (posCard2 != posCard1) {
         card2 = $(this).children().prop('src');
-        card2 = card2.substr(26);
+        card2 = card2.substr(27);
         clicks++;
         passaggio++;
       }
@@ -65,15 +70,21 @@ $(() => {
         $('img[src="img/' + card1 + '"]').parent().css({
           'opacity': '0.5'
         });
+        console.log('uguaglianza ' + card2);
+        console.log(passaggio);
         conteggioFinale++;
       } else {
         passaggio = 0;
+        console.log('disuguaglianza carta1 ' + card1 + ' carta2 ' + card2);
+        console.log(passaggio);
         setTimeout(() => {
           $('img[src="img/' + card1 + '"]').hide();
           $('img[src="img/' + card2 + '"]').hide();
         }, 600)
       }
     }
+    console.log('numero coppie azzeccate ' + conteggioFinale);
+
 
     if (conteggioFinale == 8) {
       $(".gioco").remove();
@@ -89,3 +100,27 @@ $(() => {
     location.reload();
   })
 });
+
+// quando il documento è pronto...vado a selezionare casualmente una immagine dalla cartella.
+
+// creo una variabile "images" che contiene il selettore della classe "images".
+// così ho un oggetto jQuery e metto gli elementi corrispondenti nell'oggetto jQuery.
+
+// creo un ciclo for sull'oggetto creato per ottenere poi un'immagine random.
+
+// prendo a caso un elemento dalla mia lista.
+
+// vado a prendere il file localizzato nella directory img e creo il tag html e lo inserisco nella pagina.
+//images.eq(e).html("<img id='" + e + "' src='images/" + randomImg + ".png' width='130' height='130' />");
+
+// creo la funzione principale "mostraImg"
+
+// creo l'oggetto jQuery per i clicks e lo chiamo "tuttiIClick" e prendo il valore e poi incremento.
+
+// ora visualizzo l'emoji (l'immagine) a due a due se sono diverse le nascondo altrimenti le lascio visibili.
+
+// se non sono due visualizzo l'emoji e la inserisco in "clickImgs".
+
+// se sono uguali azzero la mia lista.
+
+// se sono diverse nascondo le due immagini.
