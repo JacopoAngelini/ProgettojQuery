d853eabed313c29e16c7e0069f604d26b048d084
Jacopo Angelini <jacopo.angelini@live.it>
2022-04-08 19:15:29 +0200

    a

--- js/script.js
+++ js/script.js
@@ -41,15 +41,14 @@ $(() => {
 
 
   $('.cards').click(function() {
-    console.log('aaa');
     opacity = $(this).css('opacity');
     $(this).children().show();
 
     if (passaggio == 0 && opacity != 0.5) {
       card1 = $(this).children().prop('src');
+      console.log(card1);
       posCard1 = $(this).prop('id');
       card1 = card1.substr(91);
-      console.log(card1);
       clicks++;
       passaggio++;
     } else if (passaggio == 1 && opacity != 0.5) {
